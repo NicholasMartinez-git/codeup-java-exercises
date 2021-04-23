a07aa4a1953028e5de8684b691f3d3699d1366e1
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-04-23 11:42:52 -0500

    add the Syntax, Types, and Variables exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,70 @@ public class HelloWorld {
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
         System.out.print("Hello, World!\n");
+
+        int myFavoriteNumber = 12;
+
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Java is to JavaScript, as Ham is to Hamster";
+
+        System.out.println(myString);
+
+        //  myString = 'L'; // strings can't be single quotes
+        //  myString = 3.14159; // strings can't be numbers without quotes
+
+        // long myNumber;
+
+        // System.out.println(myNumber); // Must be initialized
+
+        // myNumber = 3.14; // long only consists of integers
+
+        // myNumber = 123L;
+        // System.out.println(myNumber);
+
+        // myNumber = 123;
+        // System.out.println(myNumber);
+
+//        float myNumber = 3.14F;
+//        System.out.println(myNumber);
+//
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        System.out.println(++x);
+//        System.out.println(x);
+
+        // String = class;
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        System.out.println(three);
+//
+//        int three = (int) "three";
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//        System.out.println(x);
+//        System.out.println(y);
+
+//        byte x = 128;
+//        x--;
+//        System.out.println(x);
+
+        byte x = 127;
+        ++x;
+        System.out.println(x);
     }
 }
