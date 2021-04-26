263b02fa8bd69a232e9a4214866d4d5c1a98e920
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-04-26 09:32:58 -0500

    finsihed the console.io exercies

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+        // 1
+//        double pi = 3.14159;
+//        System.out.printf("The value of pi is approximately %.5f.\n", pi);
+
+        // Scanner Class
+        // 1
+        Scanner sc = new Scanner(System.in);
+
+//        System.out.println("Please enter a integer: ");
+//        String userInput = sc.next();
+//        System.out.printf("You entered: %s\n", userInput);
+//
+//        System.out.println("How old are you?");
+//        int userAge = sc.nextInt();
+//        System.out.printf("Good to hear you're %s\n", userAge);
+//
+//        System.out.println("Please enter three words?");
+//        String userWord1 = sc.next();
+//        String userWord2 = sc.next();
+//        String userWord3 = sc.next();
+//        System.out.printf("You're three words were %s, %s, %s\n", userWord1, userWord2, userWord3);
+
+//        System.out.println("Please type a short sentence: ");
+//        String userSentence = sc.nextLine();
+//        System.out.printf("The sentence you wrote was: %s\n", userSentence);
+
+//        System.out.println("We will be finding the area and perimeter of the classroom.");
+//        System.out.println("Please enter the length of the classroom: ");
+//        double length = sc.nextDouble();
+//        System.out.println("Please enter the width of the classroom: ");
+//        double width = sc.nextDouble();
+//
+//        double area = length * width;
+//        double perimeter = 2 * (length + width);
+//        System.out.printf("The area of the classroom is: %.2f\n", area);
+//        System.out.printf("The perimeter of the classroom is: %.2f\n", perimeter);
+    }
+}
