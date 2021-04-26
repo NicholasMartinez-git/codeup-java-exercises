535d821c54a1be5e6179ce0ab2f53263b77204ac
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-04-26 13:53:35 -0500

    completed FizzBuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -29,13 +29,27 @@ public class ControlFlowExercises {
 //        } while (i <= 65536);
 
         // ic.
-        for (long i = 100; i >= -10; i -=5){
-            System.out.println(i);
-        }
+//        for (long i = 100; i >= -10; i -=5){
+//            System.out.println(i);
+//        }
+//
+//
+//        for(long i = 2; i < 1000000; i *= i){
+//            System.out.println(i);
+//        }
 
-
-        for(long i = 2; i < 1000000; i *= i){
-            System.out.println(i);
+        // 2
+        for (int i = 1; i <= 100; i++) {
+//            System.out.println(i);
+            if (i % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(i);
+            }
         }
     }
 }
