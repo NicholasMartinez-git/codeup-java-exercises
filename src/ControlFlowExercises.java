21472520a2bcd8c24490778e09b6b07f4084af7a
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-04-26 11:45:04 -0500

    initial commit/ completed exercise 1

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,5 +1,41 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
+        // 1a.
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.print(i + " ");
+//            i++;
+//        }
 
+        // 1b.
+
+//        int i = 0;
+//        do {
+//            i += 2;
+//
+//            System.out.print(i + " ");
+//        } while (i < 100);
+
+//        int i = 100;
+//        do {
+//            System.out.print(i + " ");
+//            i -= 5;
+//        } while (i >= -10);
+
+//        long i = 2;
+//        do {
+//            System.out.println(i);
+//            i *= i;
+//        } while (i <= 65536);
+
+        // ic.
+        for (long i = 100; i >= -10; i -=5){
+            System.out.println(i);
+        }
+
+
+        for(long i = 2; i < 1000000; i *= i){
+            System.out.println(i);
+        }
     }
 }
