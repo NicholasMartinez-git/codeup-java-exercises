8e76dfd8a7957bb0b7463bfb48b42a9407c3b529
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-04-26 15:54:42 -0500

    completed exercise 3

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -57,23 +57,27 @@ public class ControlFlowExercises {
         // 3
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("What number would you like to go up? ");
-        int upToUserInput = sc.nextInt();
+        boolean confirm;
+        do {
+            System.out.println("What number would you like to go up? ");
+            int upToUserInput = sc.nextInt();
 
-        System.out.println("Here is your table!\n");
-        System.out.println("number | squared | cubed");
-        System.out.println("------ | ------- | -----");
+            System.out.println("Here is your table!\n");
+            System.out.println("number | squared | cubed");
+            System.out.println("------ | ------- | -----");
 
-        for (int i = 1; i <= upToUserInput; i++ ) {
-            double squared = Math.pow(i, 2);
-            double cubed = Math.pow(i, 3);
-            int sq = (int) squared;
-            int cu = (int) cubed;
-            System.out.printf("%s     | %s      | %s    \n", i, sq, cu);
-        }
+            for (int i = 1; i <= upToUserInput; i++ ) {
+                double squared = Math.pow(i, 2);
+                double cubed = Math.pow(i, 3);
+                int sq = (int) squared;
+                int cu = (int) cubed;
+                System.out.printf("%s     | %s      | %s    \n", i, sq, cu);
+            }
 
-        System.out.print("Continue? [y/N]\n");
-        String userInput = sc.next();
-        boolean confirm = userInput.equalsIgnoreCase("y");
+            System.out.print("Continue? [y/N]\n");
+            String userInput = sc.next();
+            confirm = userInput.equalsIgnoreCase("y");
+
+        } while(confirm);
     }
 }
