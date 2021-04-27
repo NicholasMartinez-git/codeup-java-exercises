2b0faebd9741a8d5d91862334656349a16cdffa0
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-04-27 10:33:33 -0500

    initial commit/ Begun EX 1

---
+++ src/StringExercise.java
@@ -0,0 +1,7 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        // 1
+        String str1 = "We don't need no education\nWe don't need no thought control";
+        System.out.println(str1);
+    }
+}
