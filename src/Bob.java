50289b98fe1d225ff343712632bc5e27203e2d55
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-04-29 11:15:30 -0500

    minuscule change

--- src/Bob.java
+++ src/Bob.java
@@ -4,19 +4,21 @@ public class Bob {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); // Allows code to accept user input
 
-        System.out.println("Bob is a lackadaisical teenager." +
-                " In conversation, his responses are very limited.");
+        System.out.println("Bob is a lackadaisical teenager. " +
+                           "In conversation, his responses are very limited."); // Description of Bob
 
-        System.out.println("Would you like to talk with Bob? [y/N]");
-        String userConfirm = sc.next();
-        boolean confirm = userConfirm.equalsIgnoreCase("y");
+        System.out.println("Would you like to talk with Bob? [y/N]"); // Prompts user
+        String userConfirm = sc.next(); // Stores answer
+        boolean confirm = userConfirm.equalsIgnoreCase("y"); // Converts answer into boolean
 
         if (confirm) {
+            // If confirm = true. Run this code/conversation
             System.out.println("Bob: Hello, to whoever is reading this.");
-            System.out.println("Bob: What is your name?");
-            String userName = sc.next();
+            System.out.println("Bob: What is your name?"); // Prompts user
+            String userName = sc.next(); // Stores only first name
             System.out.printf("Hi %s, nice chatting with you.", userName);
         } else {
+            // If confirm = false. End conversation
             System.out.println("Bob: I didn't want to talk to you ether.");
         }
     }
