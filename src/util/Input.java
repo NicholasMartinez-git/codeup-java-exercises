f10a4e332047e39678c0be120dfdb316d6b8e449
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-05-01 17:58:56 -0500

    yesNo complete/ getInt() incomplete

--- src/util/Input.java
+++ src/util/Input.java
@@ -13,8 +13,23 @@ public class Input {
         return this.sc.next();
     }
 
-    public String getString(String prompt){
-        System.out.println(prompt);
-        return this.sc.next();
+    public boolean yesNo(){
+        System.out.println("please enter a string [yes/no]");
+        String answer = this.sc.next();
+        return (answer.startsWith("y"));
+    }
+
+    public int getInt(int min, int max) {
+        System.out.printf("Please enter a integer from %d to %d: %n", min, max); // Prompts question
+        int input = Integer.parseInt(sc.nextLine()); // Stores user input
+
+        // validates input is with min to max
+        if (input < min || input > max) {
+            // Re-prompts question if input is invalid
+            System.out.println("Sorry, invalid error. Try again!");
+            return getInt(min, max);
+        }
+        System.out.println("Valid Input!");
+        return input;
     }
 }
