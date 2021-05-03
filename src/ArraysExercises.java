98ed4850108c636b6b7378beef4b037464a4d4e2
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-05-03 17:34:32 -0500

    added for loop to print names

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -4,5 +4,17 @@ public class ArraysExercises {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(numbers));
+
+        // Person[] people = new Person[3];
+        Person nic = new Person("nic");
+        Person matt = new Person("matt");
+        Person jon = new Person("jon");
+        Person will = new Person("will");
+
+        Person[] people = {nic, matt, jon, will};
+
+        for (int i = 0; i < people.length; i++) {
+            System.out.println(people[i].getName());
+        }
     }
 }
