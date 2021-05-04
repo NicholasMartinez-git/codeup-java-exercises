0000fa907cf4936f6f0c27fd67447ad9aac79394
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-05-04 16:00:50 -0500

    completed rectangle setup

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,6 +1,30 @@
 package shapes;
+import java.util.Scanner;
 
 public class Rectangle {
     protected double length;
     protected double width;
+    public static Scanner sc = new Scanner(System.in);
+
+    public Rectangle() {
+        this.length = 0;
+        this.width = 0;
+    }
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getArea() {
+        return this.length * this.width;
+    }
+
+    public double getPerimeter() {
+        return (2 * this.length) + (2 * this.width);
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
