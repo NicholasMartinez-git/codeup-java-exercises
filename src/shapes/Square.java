0a9b1c6600e0e81026cde590f0dc9f3f920f4072
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-05-05 10:03:06 -0500

    initial commit/ completed Square

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,6 +1,11 @@
 package shapes;
 
-public class Square {
+public class Square extends Rectangle {
+
+    public Square(double side) {
+        super(side, side); // we use "side" as 2 parameters because rectangle method needs a length and width
+    }
+
     public static void main(String[] args) {
 
     }
