a15633ff03a6cdea984698e78e8742e1e4110fc0
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-05-07 15:41:49 -0500

    completed exercise 3

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -2,10 +2,12 @@ package grades;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Scanner; // Allows Scanner to function
 
 public class GradesApplication {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         HashMap<String, Student> students = new HashMap<>();
 
         Student nic = new Student("Nic");
@@ -40,10 +42,38 @@ public class GradesApplication {
 
         // First we declare a list that contains all of the keys of our current HashMap
         String[] listOfKeys = students.keySet().toArray(new String[0]);
-        System.out.println(Arrays.toString(listOfKeys));
+        // System.out.println(Arrays.toString(listOfKeys));
         // We create a for loop that will iterate through all keys
+        System.out.println("Welcome!\n");
+        System.out.println("Here are the GitHub usernames of our students:\n");
+
         for (String key : listOfKeys) {
-            System.out.println(key + "=" + students.get(key).getName());
+            System.out.print("|" + key + "| ");
+        }
+        System.out.println();
+
+        boolean keepGoing = true;
+
+        while (keepGoing) {
+            System.out.println("\nWhat student would you like to see more information on?");
+            String userInput = sc.next();
+            if (!students.containsKey(userInput)) {
+                System.out.printf("Sorry, no student found with the username of \"%s\"%n%n", userInput);
+            }
+            for (String key : listOfKeys) {
+                if (userInput.equalsIgnoreCase(key)) {
+                    System.out.printf("Name: %s - GitHub Username: %s%n" +
+                            "Current Average: %.2f%n", students.get(key).getName(), key, students.get(key).getGradeAverage());
+                }
+            }
+
+            System.out.println("Would you like to see another student? [y/N]");
+            String answer = sc.next();
+            keepGoing = answer.equalsIgnoreCase("y") || answer.equalsIgnoreCase("yes");
+
+            if (!keepGoing) {
+                System.out.println("\nGoodbye, and have a wonderful day!");
+            }
         }
     }
 }
